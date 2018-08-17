3d6f7d02b5d785c434c9eccd9b151104ead7ddfc
danmj <daniel.jung@stern.nyu.edu>
2018-08-17 22:22:34 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,10 +14,14 @@ def first_challenge
   }
 
   #your code here
-  data.each do |attribute, value|
-    if value == "strawberry"
-      .delete_if
-
+  contacts.each do |person, data|
+       data.each do |key, value|
+          if key == :favorite_icecream_flavors
+              value.delete_if do |flavor|
+              flavor == "strawberry"
+          end
+      end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
